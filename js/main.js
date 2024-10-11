7f93f306fb589f3a7a1e112e364470d6ce0d79ba
kyonsiii <kyonsigames@gmail.com>
2024-10-11 19:56:48 +0900

    AAA

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 //Cookie関連
+
+
+
 function setCookie(key, value, days){
     let expires = new Date();
     expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
@@ -12,4 +15,31 @@ function getCookie(key){
             .map(a => a.trim())
                 .find(c => c.startsWith(keyEQ))
                 ?.substring(keyEQ.length) || null;
+}
+
+
+function numToBit(n, mask){
+    if (n == 0) return n;
+
+    while ((mask & 0b1) != 0b1){
+        n = n << 1;
+        mask= mask >> 1;
+        if (mask == 0b0) return NaN;
+    }
+    return n;
+}
+
+
+function bitToNum(n, mask){
+    while ((mask & 0b1) != 0b1){
+        n = n >> 1;
+        mask = mask >> 1;
+        if (mask == 0b0) return NaN;
+    }
+    return n & mask;
+}
+
+    
+function bitMatch(n, mask){
+    return (n & mask) == mask;
 }
