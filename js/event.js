af176e0cf30faddbc05da37a53161750d5b159f0
kyonsiii <kyonsigames@gmail.com>
2025-07-17 13:18:56 +0900

    update event

--- js/event.js
+++ js/event.js
@@ -6,114 +6,6 @@ class PokeEvent{
         this.Youbi = {0: "日", 1: "月", 2: "火", 3: "水", 4: "木", 5: "金", 6: "土"}
         this.dataField = document.getElementById("event_schedule");
 
-        this.EventItems = {
-            /*ポケモンのピックアップ*/
-            pickup_shiny
-             : "色違い出現率アップ",
-            pickup_other_sleep_type
-             : "ほかの睡眠タイプ出現",
-            pickup_goodsleepday
-             : "ピィ/ピッピ/ピクシー 中ピックアップ",
-            pickup_undiscovered
-             : "未発見のポケモンの出現率UP",
-            pickup_darkrai
-             : "ダークライ 特別ピックアップ",
-
-            /*睡眠リサーチ関係*/
-            sleep_exp_bonus_150percent
-             : "睡眠EXP1.5倍",
-            sleep_candy_150percent
-             : "アメ獲得量1.5倍",
-            sleep_yumenokakera_150percent
-             : "リサーチのゆめのかけら1.5倍",
-            sleep_get_chance
-             : "チャンス1匹確定(1回目のみ)",
-            sleep_hunger_change
-             : "おやつで満腹になりにくい",
-
-
-            /*料理関係*/
-            cooking_capacity
-             : "なべの容量1.5倍(日曜3倍)",
-            cooking_power_150percent
-             : "料理エナジー1.5倍",
-
-            /*スキル関係*/
-            main_skill_150percent
-             : "メインスキル発動率1.5倍",
-
-            /*その他関係*/
-            mini_candy_boost
-             : "ミニアメブースト",
-            dream_gift
-             : "ドリームギフト(1日1回)",
-
-
-            /*○○関係*/
-
-            /*グッドスリープデー関係*/
-            goodsleepday_point_bonus
-             : "スリープポイントが1日前/後に+500pt、当日は+1000pt",
-            goodsleepday_exp_bonus
-             : "睡眠Expが1日前/後に2倍、当日は3倍",
-            goodsleepday_power_bonus
-             : "睡眠パワーが1日前/後に2倍、当日は3倍",
-
-
-        }
-
-        this.EventPresets = {
-             "" : {
-                items:[
-                ]
-             },
-
-            "グッドスリープデー" : {
-                items: [
-                    this.EventItems["pickup_undiscovered"],
-                    this.EventItems["pickup_goodsleepday"],
-                    this.EventItems["goodsleepday_power_bonus"],
-                    this.EventItems["goodsleepday_exp_bonus"],
-                    this.EventItems["goodsleepday_point_bonus"]
-                ]
-            },
-
-            "ニュームーンデー" : {
-                items: [
-                    this.EventItems["pickup_darkrai"],
-                    this.EventItems["pickup_shiny"],
-                    this.EventItems["sleep_hunger_change"]
-                ]
-            },
-
-            "もうすぐ2周年キャンペーン" : {
-                items: [
-                    this.EventItems["pickup_other_sleep_type"],
-                    this.EventItems["sleep_yumenokakera_150percent"],
-                    this.EventItems["cooking_capacity"],
-                    "ニャオハ・ホゲータ・クワッス 中ピックアップ"
-                ]
-            },
-
-            "2周年フェスティバル前半" : {
-                items:[
-                    this.EventItems["pickup_other_sleep_type"],
-                    this.EventItems["sleep_get_chance"],
-                    this.EventItems["main_skill_150percent"],
-                    this.EventItems["sleep_exp_bonus_150percent"],
-                    this.EventItems["sleep_candy_150percent"],
-                    this.EventItems["dream_gift"]
-                ]
-            },
-
-            "2周年フェスティバル後半" : {
-                items:[
-                    this.EventItems["pickup_other_sleep_type"],
-                    this.EventItems["cooking_power_150percent"],
-                    this.EventItems["mini_candy_boost"]
-                ]
-            }
-        }
     }
 
 
@@ -139,7 +31,7 @@ class PokeEvent{
 
 
 
-    addEntry(startDateStr, endDateStr, endAtPreviousDayEnd, presetKey, label, ...params){
+    addEntry(startDateStr, endDateStr, endAtPreviousDayEnd, presetItems, label, ...params){
         let tmpStartDate = new Date(startDateStr);
         let startDate = (this.today > tmpStartDate) ? this.today : tmpStartDate;
         let endDate = new Date(endDateStr);
@@ -153,17 +45,12 @@ class PokeEvent{
         let column = document.createElement("event_column");
         this.setDateCellsToColumn(column, 30, false);
 
-        let preset = this.EventPresets[presetKey];
-        if (preset == undefined){
-            alert(`Preset:${presetKey} is not found`);
-            return;
-        }
 
         let panel = document.createElement("event_panel");
         let titleEl = document.createElement("h3");
         let ulEl = document.createElement("ul");
         titleEl.textContent = label;
-        [...Array.from(preset.items), ...params].forEach(x =>{
+        [...Array.from(presetItems), ...params].forEach(x =>{
             let li = document.createElement("li");
             li.textContent = x;
             ulEl.appendChild(li);
