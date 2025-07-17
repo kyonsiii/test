20ff5d203dfe5bbde01aafcab5d30d37e9dc32f7
kyonsiii <kyonsigames@gmail.com>
2025-07-17 15:23:31 +0900

    update sleepTool

--- js/pokeSleepingCalc.js
+++ js/pokeSleepingCalc.js
@@ -7,7 +7,6 @@ class PokeSleepingCalc{
 
     }
     
-
     //睡眠記録関連(初期化用)
     addInputRowsToTable(){
         let calcTableEl = document.getElementById('calc_table');
@@ -16,6 +15,8 @@ class PokeSleepingCalc{
         for (let i = 1; i <= 30; i++){
             let r = tbody.insertRow();
             r.id = "calc_row_" + i;
+            r.style.display = (i < 6) ? "table-row"
+                                      : "none";
 
             let c1 = r.insertCell();
             c1.textContent = i + "回前";
@@ -23,11 +24,19 @@ class PokeSleepingCalc{
             let c2 = r.insertCell();
             let atr = "calc_row_" + ( i == 30 ? 1 : i + 1);
 
-            c2.innerHTML = '<input type="text" inputmode="decimal" class="calc_sleep_input" next_row_id="'+ atr + '" onkeydown="calc.textBoxKeyPress(this, event);"/>';
+            c2.innerHTML = '<input type="text" inputmode="decimal" class="calc_sleep_input" id=calc_row_input_' + i + ' next_row_id="'+ atr + '" onkeydown="calc.textBoxKeyPress(this, event);"/>';
             
             let c3 = r.insertCell();
             c3.innerHTML = (i == 1) ? '<button tabindex="-1" onclick="calc.offsetCalcRowValue(false);" class="smaller">↓</button> <button tabindex="-1" onclick="calc.offsetCalcRowValue(true);"  class="smaller">↑</button>'
-                                     : "";
+                         : (i == 5) ? '<span style="background-color: aquamarine;" onclick="calc.showAll();" class="smaller">6回前～を表示</span'    
+                                    : "";
+        }
+    }
+
+    showAll(){
+        for (let i = 1; i <= 30; i++){
+            var el = document.getElementById("calc_row_" + i);
+            el.style.display = "table-row";
         }
     }
 
@@ -37,51 +46,30 @@ class PokeSleepingCalc{
             nextRow.querySelector("input").focus();
             console.log(nextRow);
         }
-
     }
 
 
 
     //睡眠記録関連(ボタンで呼び出すもの)
-    showRatioCalcResult(){
+    registerNewSleepData(){
         let targTb = document.getElementById('calc_target');
-        if (targTb.value == "") {
-            this.showCurrentRatio();
+        if (targTb.value== "") return;
+        if (!this.recalcRatioOf(targTb)){
+            alert("入力値の変換に失敗しました。");
             return;
         }
 
-        this.recalcRatioOf(targTb);
-        let values = targTb.value.split('-').map(v => parseInt(v, 10));
-        let ratios = this.getCurrentRatio();
-        
-        let uto_diff  = Math.round((values[0] - ratios[0]) * 10) / 10;
-        let suya_diff = Math.round((values[1] - ratios[1]) * 10) / 10;
-        let gusu_diff = Math.round((values[2] - ratios[2]) * 10) / 10;
-        let diff_max = Math.max(uto_diff, suya_diff, gusu_diff);
-        let maxType = (diff_max == uto_diff)  ? "「うとうと」 になる可能性が高い"
-                    : (diff_max == suya_diff) ? "「すやすや」 になる可能性が高い"
-                                              : "「ぐっすり」 になる可能性が高い";
-        let mes = values[0] + "(" + this.numberToSignedNumber(uto_diff) + ") - "
-                + values[1] + "(" + this.numberToSignedNumber(suya_diff) + ") - "
-                + values[2] + "(" + this.numberToSignedNumber(gusu_diff) + ")"
-                + "\n" + maxType;  
+        this.offsetCalcRowValue();
+        let input1 = document.getElementById("calc_row_input_1");
+        input1.value = targTb.value;
 
         setCookie("srcd", this.recordsToCookieValue(), 30);
-        alert(mes);
-    }
-    
+        targTb.value = "";
+        targTb.style.backgroundColor = "LightYellow";
+        targTb.placeholder =  "登録されました。";
 
-
-    showCurrentRatio(){
-        let x = this.getCurrentRatio();
-        setCookie("srcd", this.recordsToCookieValue(), 30);
-        alert(x[0] + "-" + x[1] + "-" + x[2]);
     }
 
-
-    numberToSignedNumber(num){
-        return (num >= 0) ? "▲" + num : "▽" + Math.abs(num);
-    }
 
 
     offsetCalcRowValue(reverseMode){
@@ -99,6 +87,69 @@ class PokeSleepingCalc{
         document.getElementById('calc_row_'+ iEnd).getElementsByTagName('input')[0].value = '';
     }
 
+
+
+
+
+    getCurrentRatio(){
+        this.recalcSleepRecords();
+        let cnt  = 0;
+        let uto  = 0;
+        let suya = 0;
+        let gusu = 0;
+
+        for (let i = 1; i <= 30; i++){
+            let r = document.getElementById('calc_row_' + (i));
+            let tb = r.getElementsByTagName('input')[0];
+            
+            if (tb.value == ""){
+
+            }
+            else {
+                let values = tb.value.split('-').map(v => parseInt(v, 10));
+                uto  += values[0];
+                suya += values[1];
+                gusu += values[2];
+                cnt++;
+            }
+        }
+
+        uto  = Math.round((uto  / cnt) * 10) / 10;
+        suya = Math.round((suya / cnt) * 10) / 10;
+        gusu = Math.round((100 - uto - suya) * 10) / 10;
+        document.getElementById("calc_current_ratio").textContent = uto + "-" + suya + "-" + gusu;
+        return [uto, suya, gusu];
+    }
+
+    recalcSleepRecords(){
+        for (let i = 1; i <= 30; i++){
+            let r = document.getElementById('calc_row_' + i);
+            let tb = r.getElementsByTagName('input')[0];
+            if (tb.value != "") this.recalcRatioOf(tb);
+        }
+    }
+    
+    recalcRatioOf(tb){           
+        let tmp = tb.value.replace(/[ \._-]/g, "-");
+        let numbers = tmp.split('-').map(n => parseInt(n, 10));
+        let uto = numbers[0];
+        let suya = numbers[1];
+        let gusu = 100 - uto - suya;
+        if (gusu < 0) gusu = NaN;
+        tb.value = uto + "-" + suya + "-" + gusu;  
+        
+        return !(
+            uto  == null || isNaN(uto)  ||
+            suya == null || isNaN(suya) ||
+            gusu == null || isNaN(gusu))
+    }
+
+
+
+
+
+
+    //Cookie関連？
 
     getSleepRecordsToClipboard(){
         navigator.clipboard.writeText(this.recordsToCookieValue());
@@ -182,54 +233,40 @@ const mask_sleep_suya       = 0b000000000000000011111111;
     }
 
 
-    recalcSleepRecords(){
-        for (let i = 1; i <= 30; i++){
-            let r = document.getElementById('calc_row_' + i);
-            let tb = r.getElementsByTagName('input')[0];
-            if (tb.value != "") this.recalcRatioOf(tb);
-        }
+
+
+
+
+
+
+
+    //たぶんもう使わない
+        showRatioCalcResult(){
+        let targTb = document.getElementById('calc_target');
+        if (targTb.value == "") return;
+
+        this.recalcRatioOf(targTb);
+        let values = targTb.value.split('-').map(v => parseInt(v, 10));
+        let ratios = this.getCurrentRatio();
+        
+        let uto_diff  = Math.round((values[0] - ratios[0]) * 10) / 10;
+        let suya_diff = Math.round((values[1] - ratios[1]) * 10) / 10;
+        let gusu_diff = Math.round((values[2] - ratios[2]) * 10) / 10;
+        let diff_max = Math.max(uto_diff, suya_diff, gusu_diff);
+        let maxType = (diff_max == uto_diff)  ? "「うとうと」 になる可能性が高い"
+                    : (diff_max == suya_diff) ? "「すやすや」 になる可能性が高い"
+                                              : "「ぐっすり」 になる可能性が高い";
+        let mes = values[0] + "(" + this.numberToSignedNumber(uto_diff) + ") - "
+                + values[1] + "(" + this.numberToSignedNumber(suya_diff) + ") - "
+                + values[2] + "(" + this.numberToSignedNumber(gusu_diff) + ")"
+                + "\n" + maxType;  
+
+        setCookie("srcd", this.recordsToCookieValue(), 30);
+    }
+    
+    numberToSignedNumber(num){
+        return (num >= 0) ? "▲" + num : "▽" + Math.abs(num);
     }
 
-
-    recalcRatioOf(tb){           
-        let tmp = tb.value.replace(/[ \._-]/g, "-");
-        let numbers = tmp.split('-').map(n => parseInt(n, 10));
-        let uto = numbers[0];
-        let suya = numbers[1];
-        let gusu = 100 - uto - suya;
-        if (gusu < 0) gusu = NaN;
-        tb.value = uto + "-" + suya + "-" + gusu;     
-    }
-
-
-    getCurrentRatio(){
-        this.recalcSleepRecords();
-        let cnt  = 0;
-        let uto  = 0;
-        let suya = 0;
-        let gusu = 0;
-
-        for (let i = 1; i <= 30; i++){
-            let r = document.getElementById('calc_row_' + (i));
-            let tb = r.getElementsByTagName('input')[0];
-            
-            if (tb.value == ""){
-
-            }
-            else {
-                let values = tb.value.split('-').map(v => parseInt(v, 10));
-                uto  += values[0];
-                suya += values[1];
-                gusu += values[2];
-                cnt++;
-            }
-        }
-
-        uto  = Math.round((uto  / cnt) * 10) / 10;
-        suya = Math.round((suya / cnt) * 10) / 10;
-        gusu = Math.round((100 - uto - suya) * 10) / 10;
-        document.getElementById("calc_current_ratio").textContent = uto + "-" + suya + "-" + gusu;
-        return [uto, suya, gusu];
-    }
 
 }
